9bd48c2e77f6ddd699919f9e2f1473b83115b171
DanBurbach <dburbach1982@gmail.com>
2019-08-16 15:42:04 -0700

    set framework for basic parallax of splash main page

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -11,8 +11,21 @@ class Intro extends Component {
 
   render() {
     return (
-      <div className='container_intro'>
-        <Welcome/>
+      <div className="container_intro">
+        <div className='parallax'>
+        <div className="parallax_container">
+          <div className="parallax_layer p1" />
+          <div className="parallax_layer p2" />
+          <div className="parallax_layer p3" />
+          <div className="parallax_layer p4">
+            <Welcome />
+          </div>
+          <div className="parallax_layer p5" />
+          <div className="parallax_layer p6">
+            <div id="intro_content" />
+          </div>
+        </div>
+        </div>
       </div>
     );
   }
