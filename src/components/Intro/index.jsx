44cdd233ca89e14bb9db0a3ca183ab216f6ee63b
DanBurbach <dburbach1982@gmail.com>
2019-08-16 22:34:34 -0700

    impliment parallax for style, working on shrinking overall height of parallax box

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 import Welcome from './Welcome';
-import "./../../assets/Intro.css";
+import './../../assets/Intro.css';
 
 class Intro extends Component {
   constructor(props) {
@@ -11,19 +11,20 @@ class Intro extends Component {
 
   render() {
     return (
-      <div className="container_intro">
+      <div className='container_intro'>
         <div className='parallax'>
-        <div className="parallax_container">
-          <div className="parallax_layer p1" />
-          <div className="parallax_layer p2" />
-          <div className="parallax_layer p3" />
-          <div className="parallax_layer p4">
+        <div className='parallax_container'>
+          <div className='parallax_layer p1'/>
+          <div className='parallax_container'></div>
+          <div className='parallax_layer p2'/>
+          <div className='parallax_container'></div>
+          <div className='parallax_layer p3'/>
+          <div className='parallax_container'></div>
+          <div className='parallax_layer p4'>
             <Welcome />
           </div>
-          <div className="parallax_layer p5" />
-          <div className="parallax_layer p6">
-            <div id="intro_content" />
-          </div>
+          <div className='parallax_layer p5'/>
+            <div id='intro_content' />
         </div>
         </div>
       </div>
