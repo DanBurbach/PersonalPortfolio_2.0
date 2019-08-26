ff57c5e68453f26a8f1d2f44b87fbf00392f3ee6
DanBurbach <dburbach1982@gmail.com>
2019-08-26 11:30:09 -0700

    begin work on layout change up of Projects from verticle layout to grid

--- src/components/Projects/ProjectsInfo.jsx
+++ src/components/Projects/ProjectsInfo.jsx
@@ -8,11 +8,13 @@ function ProjectsInfo(props){
   return (
     <div>
         <div className="container_projectsInfo">
-          <h4>{props.projectname}</h4>
-          <p><em>{props.description}</em></p>
-          <img src= {props.screenimage} alt='screenshot' height='175em'/>
-          <p><em>{props.language}</em></p>
-          <a href={props.link}>Link To Project</a>
+          <div className='col-lg-4'>
+            <h4>{props.projectname}</h4>
+            <p><em>{props.description}</em></p>
+            <img src= {props.screenimage} alt='screenshot' height='175em'/>
+            <p><em>{props.language}</em></p>
+            <a href={props.link}>Link To Project</a>
+          </div>
         </div>
       </div>
   );
