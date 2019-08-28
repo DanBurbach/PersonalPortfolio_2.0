91a30dd1a6bb8d6d231c3fade401911ee7e327be
DanBurbach <dburbach1982@gmail.com>
2019-08-28 00:24:09 -0700

    change sizing and layout of background clouds and balloons

--- src/components/Skills/Balloon.jsx
+++ src/components/Skills/Balloon.jsx
@@ -10,18 +10,46 @@ class Balloon extends Component {
 
   render() {
     return (
-      <figure className="balloon">
-        <div className="envelope">
-          <span />
-          <span />
-        </div>
-        <div className="basket">
-          <span />
-          <span />
-          <span />
-          <span />
-        </div>
-      </figure>
+      <div>
+        <figure className="balloon">
+          <div className="envelope">
+            <span />
+            <span />
+          </div>
+          <div className="basket">
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+        </figure>
+        
+        <figure className="balloon_2">
+          <div className="envelope_2">
+            <span />
+            <span />
+          </div>
+          <div className="basket_2">
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+        </figure>
+
+        <figure className="balloon_3">
+  `        <div className="envelope_3">
+            <span />
+            <span />
+          </div>
+          <div className="basket_3">
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>`
+        </figure>
+      </div>
     );
   }
 }
