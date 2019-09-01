0429622e0f87b6ada788c31665e5d960b764f664
DanBurbach <dburbach1982@gmail.com>
2019-08-31 21:15:26 -0700

    add Coding Against Humility to Project list AND change styling to allow for responsive layout depending on screen size/resolution

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './../../assets/Projects.css';
 
-const elements = document.getElementsByClassName("column");
-let i;
 
 class Projects extends Component {
   constructor(props) {
@@ -10,86 +8,84 @@ class Projects extends Component {
     this.state = {};
   }
 
-  handleListView = () => {
-    for (i = 0; i < elements.length; i++){
-      elements[i].style.width = "100%";
-    }
-  }
-
-   handleGridView = () => {
-    for (i = 0; i < elements.length; i++){
-      elements[i].style.width = "50%";
-    }
-  }
-
   render() {
     return (
-      <div>
-        <button onClick={this.handleListView}>
-          <i className="fa fa-bars" /> List
-        </button>
-        <button onClick={this.handleGridView}>
-          <i className="fa fa-th-large" /> Grid
-        </button>
-
-        <div className="row">
-          <div className="column" styles="background-color:#aaa;">
-            <h2>Vacation Quiz</h2>
-            <img source="https://github.com/DanBurbach/PersonalPortfolio/blob/master/src/assets/images/vacationquizscreenshot.png?raw=true" alt='Vacation Quiz'/>
-            <p>
-              A webpage using input from the user and radio buttons to
-              provide recommended vacation sites
-            </p>
-            <p>Javascript, HTML, CSS, and jQuery</p>
-            'https://vacation-quiz.herokuapp.com/index.html'
-          </div>
-          <div className="column" styles="background-color:#bbb;">
-            <h2>Social Media App</h2>
-            <img source="https://github.com/DanBurbach/PersonalPortfolio/blob/master/src/assets/images/SocialMediaGame.jpeg?raw=true" alt='Social Media App'/>
-            <p>
-              A group project: This site was designed so that users can
-              choose specific media types to view - Text, Image, or Video;
-              and share these with other users. The site uses 3 different
-              APIs to mimic user posts for presentation purposes.
-            </p>
-            <p>Typescript, Angular, Firebase database, CSS, HTML</p>
-            'https://github.com/DanBurbach/SocialMediaGame_Main'
-          </div>
+      <div className="gallery">
+        <div className="galleryItem">
+          <h2>Coding Against Humility</h2>
+          <a href="https://github.com/DanBurbach/Coding-Against-Humility">
+            <img
+              src="https://raw.githubusercontent.com/DanBurbach/Coding-Against-Humility/master/src/assets/images/CAH_MainScreen.png"
+              alt="Coding Against Humility"
+            />
+          </a>
+          <p>
+            A game based off of a hit cardgame. Uses two JSON files for
+            white and black cards. Authentication through Firebase, heavy
+            use of CSS for animation of black card flip and white card hand,
+            and use of drag and drop.
+          </p>
+          <p>React.js, Redux, CSS3, HTML5, and Firebase Database</p>
+        </div>
+        <div className="galleryItem">
+          <h2>Vacation Quiz</h2>
+          <a href="https://vacation-quiz.herokuapp.com/index.html">
+            <img
+              src="https://github.com/DanBurbach/PersonalPortfolio/blob/master/src/assets/images/vacationquizscreenshot.png?raw=true"
+              alt="Vacation Quiz"
+            />
+          </a>
+          <p>
+            A webpage using input from the user and radio buttons to provide
+            recommended vacation sites.
+          </p>
+          <p>Javascript, CSS3, and HTML5</p>
+        </div>
+        <div className="galleryItem">
+          <h2>Social Media App</h2>
+          <a href="https://github.com/DanBurbach/SocialMediaGame_Main">
+            <img
+              src="https://github.com/DanBurbach/PersonalPortfolio/blob/master/src/assets/images/SocialMediaGame.jpeg?raw=true"
+              alt="Social Media App"
+            />
+          </a>
+          <p>
+            A group project: This site was designed so that users can choose
+            specific media types to view - Text, Image, or Video; and share
+            these with other users. The site uses 3 different APIs to mimic
+            user posts for presentation purposes.
+          </p>
+          <p>Typescript, Angular, Firebase Database, CSS3, and HTML5</p>
         </div>
 
-        <div className="row">
-          <div className="column" styles="background-color:#ccc;">
-            <h2>Battleship!</h2>
-            <img src="https://github.com/DanBurbach/PersonalPortfolio/blob/master/src/assets/images/BattleShipRuby.png?raw=true" alt='Battleship!'/>
-            <p>
-              A team project build in Ruby, this is a simple game of
-              battleship where two players try to sink one anothers ships.
-            </p>
-            <p>Ruby 2.5.1, Rails 5, HTML, CSS, PostgreSQL, Bycrpt</p>
-            'https://github.com/DanBurbach/battle-ships'
-          </div>
-          <div className="column" styles="background-color:#ddd;">
-            <h2>Blind Date Simulator</h2>
-            <img src="https://github.com/DanBurbach/PersonalPortfolio/blob/master/src/assets/images/BlindDateSimulator.png?raw=true" alt='Blind Date Simulator'/>
-            <p>
-              A webpage that takes you through a blind date where you
-              navigate your way through a series of questions, all leading
-              to various results.'
-            </p>
-            <p>Javascript, Bootstrap, HTML, CSS, and jQuery</p>
-            'https://blind-date-simulator.herokuapp.com/index.html'
-          </div>
+        <div className="galleryItem">
+          <h2>Battleship!</h2>
+          <a href="https://github.com/DanBurbach/battle-ships">
+            <img
+              src="https://github.com/DanBurbach/PersonalPortfolio/blob/master/src/assets/images/BattleShipRuby.png?raw=true"
+              alt="Battleship!"
+            />
+          </a>
+          <p>
+            A team project build in Ruby, this is a simple game of
+            battleship where two players try to sink one anothers ships.
+          </p>
+          <p>Ruby 2.5.1, Rails 5, HTML5, CSS3, PostgreSQL, Bycrpt</p>
         </div>
-
-        <div className="row">
-          <div className="column" styles="background-color:#ccc;">
-            <h2>Column 5</h2>
-            <p>Some text..</p>
-          </div>
-          <div className="column" styles="background-color:#ddd;">
-            <h2>Column 6</h2>
-            <p>Some text..</p>
-          </div>
+        <div className="galleryItem">
+          <h2>Blind Date Simulator</h2>
+          <a href="https://blind-date-simulator.herokuapp.com/index.html">
+            <img
+              src="https://github.com/DanBurbach/PersonalPortfolio/blob/master/src/assets/images/BlindDateSimulator.png?raw=true"
+              alt="Blind Date Simulator"
+            />
+          </a>
+          <p>
+            A webpage that takes you through a blind date where you navigate
+            your way through a series of questions, all leading to various
+            results.
+          </p>
+          <p>Javascript, Bootstrap, HTML5, CSS3, and jQuery</p>
         </div>
       </div>
     );
