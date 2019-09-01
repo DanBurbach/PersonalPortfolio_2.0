9806b7efe8e4f58523b49a17e4320895990c765f
DanBurbach <dburbach1982@gmail.com>
2019-08-31 21:16:23 -0700

    correct 'class' call to proper 'className' instead

--- src/components/About/Timeline.jsx
+++ src/components/About/Timeline.jsx
@@ -12,10 +12,10 @@ class Timeline extends Component {
 
   render() {
     return (
-      <div class="timeline_container">
-        <div class="timeline-block timeline-block-right">
-          <div class="marker" />
-          <div class="timeline-content">
+      <div className="timeline_container">
+        <div className="timeline-block timeline-block-right">
+          <div className="marker" />
+          <div className="timeline-content">
             <h3>1987</h3>
             <span>
               First time using any language: DOS. Played "Donald Ducks's
@@ -24,33 +24,33 @@ class Timeline extends Component {
           </div>
         </div>
 
-        <div class="timeline-block timeline-block-left">
-          <div class="marker" />
-          <div class="timeline-content">
+        <div className="timeline-block timeline-block-left">
+          <div className="marker" />
+          <div className="timeline-content">
             <h3>1991</h3>
             <span>First time using a Mac. "Oregon Trail"</span>
           </div>
         </div>
 
-        <div class="timeline-block timeline-block-right">
-          <div class="marker" />
-          <div class="timeline-content">
+        <div className="timeline-block timeline-block-right">
+          <div className="marker" />
+          <div className="timeline-content">
             <h3>1997</h3>
             <span>Use of MasterCAM Mill in high school engineering</span>
           </div>
         </div>
 
-        <div class="timeline-block timeline-block-left">
-          <div class="marker" />
-          <div class="timeline-content">
+        <div className="timeline-block timeline-block-left">
+          <div className="marker" />
+          <div className="timeline-content">
             <h3>1999</h3>
             <span>Adobe Photoshop for high school photo journalism</span>
           </div>
         </div>
 
-        <div class="timeline-block timeline-block-right">
-          <div class="marker" />
-          <div class="timeline-content">
+        <div className="timeline-block timeline-block-right">
+          <div className="marker" />
+          <div className="timeline-content">
             <h3>2000 - 2001</h3>
             <span>
               Clackamas Community College. Studied history,
@@ -58,9 +58,9 @@ class Timeline extends Component {
             </span>
           </div>
         </div>
-        <div class="timeline-block timeline-block-left">
-          <div class="marker" />
-          <div class="timeline-content">
+        <div className="timeline-block timeline-block-left">
+          <div className="marker" />
+          <div className="timeline-content">
             <h3>2001 - 2005</h3>
             <span>
               University of Oregon. Focus on Political Science and
@@ -70,18 +70,18 @@ class Timeline extends Component {
           </div>
         </div>
 
-        <div class="timeline-block timeline-block-right">
-          <div class="marker" />
-          <div class="timeline-content">
+        <div className="timeline-block timeline-block-right">
+          <div className="marker" />
+          <div className="timeline-content">
             <h3>2006</h3>
             <span>
               First job out of college as website developer/designer for flower delivery company - Reed & Cross - in Eugene, OR
             </span>
           </div>
         </div>
-        <div class="timeline-block timeline-block-left">
-          <div class="marker" />
-          <div class="timeline-content">
+        <div className="timeline-block timeline-block-left">
+          <div className="marker" />
+          <div className="timeline-content">
             <h3>2007 - 2008</h3>
             <span>
               Americorps with Habitat for Humanity in New Orleans, LA and
@@ -90,50 +90,50 @@ class Timeline extends Component {
           </div>
         </div>
 
-        <div class="timeline-block timeline-block-right">
-          <div class="marker" />
-          <div class="timeline-content">
+        <div className="timeline-block timeline-block-right">
+          <div className="marker" />
+          <div className="timeline-content">
             <h3>2009 - 2013</h3>
             <span>American Tinnitus Association. Fulfillment Coordinator and Social Media Marketing Manager</span>
           </div>
         </div>
-        <div class="timeline-block timeline-block-left">
-          <div class="marker" />
-          <div class="timeline-content">
+        <div className="timeline-block timeline-block-left">
+          <div className="marker" />
+          <div className="timeline-content">
             <h3>2013 - 2018</h3>
             <span>ACME Scenic and Display. Installer I and On-Site Project Manager</span>
           </div>
         </div>
 
-        <div class="timeline-block timeline-block-right">
-          <div class="marker" />
-          <div class="timeline-content">
+        <div className="timeline-block timeline-block-right">
+          <div className="marker" />
+          <div className="timeline-content">
             <span>Some work experience</span>
           </div>
         </div>
-        <div class="timeline-block timeline-block-left">
-          <div class="marker" />
-          <div class="timeline-content">
-            <span>Some work experience</span>
-          </div>
-        </div>
-
-        <div class="timeline-block timeline-block-right">
-          <div class="marker" />
-          <div class="timeline-content">
-            <span>Some work experience</span>
-          </div>
-        </div>
-        <div class="timeline-block timeline-block-left">
-          <div class="marker" />
-          <div class="timeline-content">
+        <div className="timeline-block timeline-block-left">
+          <div className="marker" />
+          <div className="timeline-content">
             <span>Some work experience</span>
           </div>
         </div>
 
-        <div class="timeline-block timeline-block-right">
-          <div class="marker" />
-          <div class="timeline-content">
+        <div className="timeline-block timeline-block-right">
+          <div className="marker" />
+          <div className="timeline-content">
+            <span>Some work experience</span>
+          </div>
+        </div>
+        <div className="timeline-block timeline-block-left">
+          <div className="marker" />
+          <div className="timeline-content">
+            <span>Some work experience</span>
+          </div>
+        </div>
+
+        <div className="timeline-block timeline-block-right">
+          <div className="marker" />
+          <div className="timeline-content">
             <span>Some work experience</span>
           </div>
         </div>
