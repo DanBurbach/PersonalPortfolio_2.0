9d8bc6b6f38c0bdfc29726d846ee8371ac6014e2
DanBurbach <dburbach1982@gmail.com>
2019-09-03 23:48:03 -0700

    upload social media icons, layout using Flexbox in CSS3, and adjust title of contact form

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Form from "./Form.jsx";
+import "./../../assets/Contact.css";
 
 
 class Contact extends Component {
@@ -10,10 +11,50 @@ class Contact extends Component {
 
   render() {
     return (
-      <div>
-        <Form/>
+      <div className="contact_flex">
+        <div>
+          <div className="icon_layout">
+            <a href="https://www.linkedin.com/in/danielburbach/">
+              <img
+                src={require("./../../assets/icons/linkedinIcon.svg")}
+                alt="LinkedIn"
+              />
+            </a>
+          </div>
+          <div className="icon_layout">
+            <div className="github_icon_background">
+              <a href="https://github.com/DanBurbach">
+                <img
+                  src={require("./../../assets/icons/githubIcon.svg")}
+                  alt="GitHub"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <Form />
+
+        <div>
+          <div className="icon_layout">
+            <a href="https://www.pinterest.com/dannodemo/">
+              <img
+                src={require("./../../assets/icons/pinterestIcon.svg")}
+                alt="Pintrest"
+              />
+            </a>
+          </div>
+          <div className="icon_layout">
+            <a href="https://twitter.com/DannoDemo">
+              <img
+                src={require("./../../assets/icons/twitterIcon.svg")}
+                alt="Twitter"
+              />
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 export default Contact;
