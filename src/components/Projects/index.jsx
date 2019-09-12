237d0319221848fb51eda48a3ad5dd0f5530cee6
DanBurbach <dburbach1982@gmail.com>
2019-09-11 22:49:37 -0700

    add additional project of Rick and Morty API to Project lineup

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -20,12 +20,26 @@ class Projects extends Component {
             />
           </a>
           <p>
-            A game based off of a hit cardgame. Uses two JSON files for
-            white and black cards. Authentication through Firebase, heavy
-            use of CSS for animation of black card flip and white card hand,
-            and use of drag and drop.
+            A game based off of a hit cardgame. Uses two JSON files for white
+            and black cards. Authentication through Firebase, heavy use of CSS
+            for animation of black card flip and white card hand, and use of
+            drag and drop.
           </p>
           <p>React.js, Redux, CSS3, HTML5, and Firebase Database</p>
+        </div>
+        <div className="galleryItem">
+          <h2>Rick and Morty API</h2>
+          <a href="https://github.com/DanBurbach/rick-and-morty-api-react">
+            <img
+              src="https://github.com/DanBurbach/rick-and-morty-api-react/raw/master/src/assets/Rick&MortyScreenShot.png?raw=true"
+              alt="Rick and Morty API Project"
+            />
+          </a>
+          <p>
+            A project that uses a non-password API call to return searched for
+            data. Library contains characters from "Rick and Morty".
+          </p>
+          <p>React.js, Redux, CSS3, HTML5, and API calls</p>
         </div>
         <div className="galleryItem">
           <h2>Vacation Quiz</h2>
@@ -52,8 +66,8 @@ class Projects extends Component {
           <p>
             A group project: This site was designed so that users can choose
             specific media types to view - Text, Image, or Video; and share
-            these with other users. The site uses 3 different APIs to mimic
-            user posts for presentation purposes.
+            these with other users. The site uses 3 different APIs to mimic user
+            posts for presentation purposes.
           </p>
           <p>Typescript, Angular, Firebase Database, CSS3, and HTML5</p>
         </div>
@@ -67,8 +81,8 @@ class Projects extends Component {
             />
           </a>
           <p>
-            A team project build in Ruby, this is a simple game of
-            battleship where two players try to sink one anothers ships.
+            A team project build in Ruby, this is a simple game of battleship
+            where two players try to sink one anothers ships.
           </p>
           <p>Ruby 2.5.1, Rails 5, HTML5, CSS3, PostgreSQL, Bycrpt</p>
         </div>
