2ea30efa3144767cc239bf8392ca111f91dd8a78
DanBurbach <dburbach1982@gmail.com>
2019-09-11 22:46:40 -0700

    add beginning framework, images, and style sheet for project parallax background

---
+++ src/components/Projects/Parallax_Wallpaper.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './../../assets/Parallax_Wallpaper.css';
+
+
+class Parallax_Wallpaper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+        <div>
+
+        </div>
+        );
+    }
+}
+
+
+export default Parallax_Wallpaper;
