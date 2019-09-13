334421bce6fd13a409cc794843557cee50fd8ca5
DanBurbach <dburbach1982@gmail.com>
2019-09-13 00:11:36 -0700

    add mouse reactivity with parallax right about contact footer

--- src/components/Projects/Parallax_Wallpaper.jsx
+++ src/components/Projects/Parallax_Wallpaper.jsx
@@ -1,39 +1,51 @@
 import React, { Component } from 'react';
+import Parallax from "parallax-js";
 import './../../assets/Parallax_Wallpaper.css';
 
-
-class Parallax_Wallpaper extends Component {
+class ParallaxWallpaper extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
+  componentDidMount() {
+    this.parallax = new Parallax(this.scene);
+  }
+  componentWillUnmount() {
+    this.parallax.disable();
+  }
+
   render() {
     return (
-      <div class="parallax">
-        <div class="parallaxLayer layer_1">
-          <img src="https://github.com/DanBurbach/PersonalPortfolio_2.0/blob/master/src/assets/parallax_images/layer_1.png?raw=true" />
+      <div>
+        <div className="parallax">
+          <div data-pointer-events="true" id="scene">
+            <ul ref={el => (this.scene = el)}>
+              <li className="parallaxLayer" data-depth="1.50" id="p1" />
+              <li className="parallaxLayer" data-depth="1.00" id="p3">
+                <img src="https://github.com/DanBurbach/PersonalPortfolio_2.0/blob/master/src/assets/parallax_images/layer_2.png?raw=true" alt="p3"/>
+              </li>
+              <li className="parallaxLayer" data-depth="0.75" id="p4">
+                <img src="https://github.com/DanBurbach/PersonalPortfolio_2.0/blob/master/src/assets/parallax_images/layer_3.png?raw=true" alt="p4"/>
+              </li>
+              <li className="parallaxLayer" data-depth="0.5" id="p5">
+                <img src="https://github.com/DanBurbach/PersonalPortfolio_2.0/blob/master/src/assets/parallax_images/layer_4.png?raw=true" alt="p5"/>
+              </li>
+              <li className="parallaxLayer" data-depth="0.25" id="p2">
+                <img src="https://github.com/DanBurbach/PersonalPortfolio_2.0/blob/master/src/assets/parallax_images/layer_5.png?raw=true" alt="p2"/>
+              </li>
+
+              <li className="parallaxLayer" data-depth="0.00" id="p6">
+                <img src="https://github.com/DanBurbach/PersonalPortfolio_2.0/blob/master/src/assets/parallax_images/layer_6.png?raw=true" alt="p6"/>
+              </li>
+            </ul>
+          </div>
+          <div className="parallax__cover" data-depth="0.00"></div>
         </div>
-        <div class="parallaxLayer layer_2">
-          <img src="https://github.com/DanBurbach/PersonalPortfolio_2.0/blob/master/src/assets/parallax_images/layer_2.png?raw=true" />
-        </div>
-        <div class="parallaxLayer layer_3">
-          <img src="https://github.com/DanBurbach/PersonalPortfolio_2.0/blob/master/src/assets/parallax_images/layer_4.png?raw=true" />
-        </div>
-        <div class="parallaxLayer layer_4">
-          <img src="https://github.com/DanBurbach/PersonalPortfolio_2.0/blob/master/src/assets/parallax_images/layer_5.png?raw=true" />
-        </div>
-        <div class="parallaxLayer layer_5">
-          <img src="https://github.com/DanBurbach/PersonalPortfolio_2.0/blob/master/src/assets/parallax_images/layer_6.png?raw=true" />
-        </div>
-        <div class="parallaxLayer layer_6">
-          <img src="https://github.com/DanBurbach/PersonalPortfolio_2.0/blob/master/src/assets/parallax_images/layer_6.png?raw=true" />
-        </div>
-        <div class="parallax__cover"></div>
       </div>
     );
   }
 }
 
 
-export default Parallax_Wallpaper;
+export default ParallaxWallpaper;
